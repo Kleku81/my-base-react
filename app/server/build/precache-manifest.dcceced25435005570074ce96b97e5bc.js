55a885d990acc72aa1a75477bf098ec4a2ae99f1
Marcin Klekowiecki <test@test.com>
2021-02-18 19:53:42 +0100

    after correct  branch expand v2

---
+++ app/server/build/precache-manifest.dcceced25435005570074ce96b97e5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab42735fea8e2ad4f55f60eaff14e7d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0c454214118c6aadbd",
+    "url": "/static/css/2.a8d3df93.chunk.css"
+  },
+  {
+    "revision": "da3133b780b5567fd944",
+    "url": "/static/css/main.5c0a64c5.chunk.css"
+  },
+  {
+    "revision": "6d0c454214118c6aadbd",
+    "url": "/static/js/2.aa73c65f.chunk.js"
+  },
+  {
+    "revision": "1cd0c55d9b362b6f843944eb5e03b75a",
+    "url": "/static/js/2.aa73c65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3133b780b5567fd944",
+    "url": "/static/js/main.fc346d48.chunk.js"
+  },
+  {
+    "revision": "2da79d0f5fb1eed8230e",
+    "url": "/static/js/runtime-main.20fd96de.js"
+  }
+]);
--- app/server/build/service-worker.js
+++ app/server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.354d7dc52c7925adc8d895361b871b0b.js"
+  "/precache-manifest.dcceced25435005570074ce96b97e5bc.js"
 );
 
 self.addEventListener('message', (event) => {
